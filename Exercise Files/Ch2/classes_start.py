31f2a0904910f3970279993bf9cd5dc48b1c9e87
CarolLiu <carol.gugu@hotmail.com>
2020-09-23 17:49:18 +1000

    python basic comple

--- Exercise Files/Ch2/classes_start.py
+++ Exercise Files/Ch2/classes_start.py
@@ -5,9 +5,29 @@
 
 class myClass():
     def method1(self):
+        print("myClass method1")
+
+    def method2(self, someStr):
+        print("myClass method2 " + someStr)
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someStr):
+        print("anotherClass method2 ")
 
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("This is a string")
 
 
 if __name__ == "__main__":
