a973269444567a00f97870f90f6768d489be53dc
CarolLiu <carol.gugu@hotmail.com>
2020-09-23 18:16:57 +1000

    Formatting time output

--- Exercise Files/Ch3/formatting_start.py
+++ Exercise Files/Ch3/formatting_start.py
@@ -4,23 +4,37 @@
 
 from datetime import datetime
 
+
 def main():
-  # Times and dates can be formatted using a set of predefined string
-  # control codes 
+    # Times and dates can be formatted using a set of predefined string
+    # control codes
+    now = datetime.now()
+    #### Date Formatting ####
 
-  #### Date Formatting ####
-  
-  # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+    # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+    print(now.strftime("The current year is: %Y"))
+    print(now.strftime("%a, %d %B, %y"))
 
+    # %c - locale's date and time, %x - locale's date, %X - locale's time
+    print(now.strftime("Locale date and time: %c"))
+    print(now.strftime("Locale date: %x"))
+    print(now.strftime("Locale time: %X"))
 
-  # %c - locale's date and time, %x - locale's date, %X - locale's time
+    #### Time Formatting ####
 
-
-  #### Time Formatting ####
-  
-  # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
-
+    # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+    print(now.strftime("Current time: %I:%M:%S %p"))
+    print(now.strftime("24-hour time: %H:%M"))
 
 
 if __name__ == "__main__":
-  main();
+    main()
+
+
+# The current year is: 2020
+# Wed, 23 September, 20
+# Locale date and time: Wed Sep 23 18:13:39 2020
+# Locale date: 09/23/20
+# Locale time: 18:13:39
+# Current time: 06:13:39 PM
+# 24-hour time: 18:13
